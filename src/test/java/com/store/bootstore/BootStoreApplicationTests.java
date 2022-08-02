def459fd519b6444ceec812fcbaa2e8160beaa8f
LAPTOP-G83GLVK3\zjp <gaara_zjp@163.com>
2022-08-02 12:58:08 +0800

    Initial commit

---
+++ src/test/java/com/store/bootstore/BootStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.bootstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
